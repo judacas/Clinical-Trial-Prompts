f001ef3d11026caa9fd69632870207d9c9e6f971
Daniel Carvajal <dcarvajal@gmail.com>
2023-09-22 00:41:50 -0400

    Cleaned up repository

--- Code/testingStuff.py
+++
@@ -1,8 +0,0 @@
-import json
-
-json_file_path = "clinical_trials_simplified.json"
-
-# Load the JSON data from the file
-with open(json_file_path, encoding="utf-8") as f:
-    data = f.read()
-    print(data)
