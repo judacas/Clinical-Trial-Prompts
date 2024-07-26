f5ad22b664fd67a5e4099fd44d59146d81e15536
Daniel Carvajal <dcarvajal@gmail.com>
2024-07-26 18:42:09 -0400

    started the ability to talk to trials until I realized I should just stick to the old only one trial boolean talker until new structure completed

---
+++ Code/Criterion.py
@@ -0,0 +1,8 @@
+
+# Will be expanded on a lot once modifiers structure gets implemented
+class Criterion:
+    def __init__(self, text, answers, nctID):
+        self.text = text
+        self.answers = answers
+        self.nctID = nctID
+    
