4d2e176fe2c9f4cadbb60f17a1abe80b02bd6b47
Daniel Carvajal <dcarvajal@gmail.com>
2024-12-19 15:17:17 -0500

    clean up repo

--- Code/deprecated/Criterion.py
+++
@@ -1,8 +0,0 @@
-
-# Will be expanded on a lot once modifiers structure gets implemented
-class Criterion:
-    def __init__(self, text, answers, nctID):
-        self.text = text
-        self.answers = answers
-        self.nctID = nctID
-    
